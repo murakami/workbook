1e07f49035148416b262f93973ee4fb7470678c1
村上 幸雄 <yukio@mbc2d.local>
2012-01-21 23:16:59 +0900

    2012/01/21

---
+++ mac/hosttime/hosttime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hosttime
+//
+//  Created by 村上 幸雄 on 12/01/21.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
