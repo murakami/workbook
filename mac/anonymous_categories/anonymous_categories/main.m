045fb0fb33f524de823db76788154ada7eb0863a
村上 幸雄 <yukio@mbc2d.local>
2012-01-25 23:11:26 +0900

    2012/01/25

---
+++ mac/anonymous_categories/anonymous_categories/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  anonymous_categories
+//
+//  Created by 村上 幸雄 on 12/01/25.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
