40c95b64d094cd3fdd82213f67b61f57aec1ad51
村上 幸雄 <yukio@mbc2d.local>
2012-01-26 00:01:11 +0900

    2012/01/26

--- mac/anonymous_categories/anonymous_categories/main.m
+++ mac/anonymous_categories/anonymous_categories/main.m
@@ -8,15 +8,45 @@
 
 #import <Foundation/Foundation.h>
 
+@interface MyClass : NSObject
+@property (nonatomic, strong, readonly) NSMutableString   *string;
+- (void)publicMethod;
+@end
+
+@interface MyClass ()
+@property (nonatomic, strong, readwrite) NSMutableString   *string;
+- (void)privateMethod;
+@end
+
+@implementation MyClass
+
+@synthesize string = _string;
+
+- (void)publicMethod
+{
+    NSLog(@"%s", __func__);
+    _string = [[NSMutableString alloc] initWithString:@"hello, world!"];
+    [self privateMethod];
+}
+
+- (void)privateMethod
+{
+    NSLog(@"%s", __func__);
+    NSLog(@"self.string: %@", self.string);
+}
+
+@end
+
 int main (int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        MyClass *instance = [[MyClass alloc] init];
+        [instance publicMethod];
         
     }
     return 0;
 }
 
+/* End Of File */
