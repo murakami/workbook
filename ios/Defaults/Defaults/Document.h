4d8ee7e04eecbcf3e508294145a3451b0d521b76
murakami <m_yukio@mac.com>
2012-01-30 20:57:24 +0900

    2012/01/30

---
+++ ios/Defaults/Defaults/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  Defaults
+//
+//  Created by 村上 幸雄 on 12/01/30.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Document : NSObject
+- (void)clearDefaults;
+- (void)updateDefaults;
+- (void)loadDefaults;
+@end
