2afd28090020411874d676e3c0fc97129f6a9e9f
murakami <m_yukio@mac.com>
2012-02-04 00:07:52 +0900

    2012/2/4

--- ios/LocalNotification/LocalNotification/AppDelegate.m
+++ ios/LocalNotification/LocalNotification/AppDelegate.m
@@ -47,7 +47,7 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
-    NSLog(@"Application entered background state.");
+    DBGMSG(@"Application entered background state.");
     NSAssert(self.bgTask == UIBackgroundTaskInvalid, nil);
     self.bgTask = [application beginBackgroundTaskWithExpirationHandler: ^{
         dispatch_async(dispatch_get_main_queue(), ^{
