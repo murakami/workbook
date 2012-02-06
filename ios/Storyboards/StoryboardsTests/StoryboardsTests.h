c8c8864b9341dd839632d52af2229ac6f44d7c98
murakami <m_yukio@mac.com>
2012-02-06 23:33:36 +0900

    Storyboards

---
+++ ios/Storyboards/StoryboardsTests/StoryboardsTests.h
@@ -0,0 +1,13 @@
+//
+//  StoryboardsTests.h
+//  StoryboardsTests
+//
+//  Created by 村上 幸雄 on 12/02/06.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StoryboardsTests : SenTestCase
+
+@end
---
+++ ios/Storyboards/StoryboardsTests/StoryboardsTests.m
@@ -0,0 +1,32 @@
+//
+//  StoryboardsTests.m
+//  StoryboardsTests
+//
+//  Created by 村上 幸雄 on 12/02/06.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "StoryboardsTests.h"
+
+@implementation StoryboardsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StoryboardsTests");
+}
+
+@end
