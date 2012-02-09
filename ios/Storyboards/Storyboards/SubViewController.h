0de69d4813e55c3ab4d44fb108e7d72c7905efb4
murakami <m_yukio@mac.com>
2012-02-09 21:34:19 +0900

    2012/02/09

---
+++ ios/Storyboards/Storyboards/SubViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubViewController.h
+//  Storyboards
+//
+//  Created by 村上 幸雄 on 12/02/09.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubViewController : UIViewController
+
+@end
