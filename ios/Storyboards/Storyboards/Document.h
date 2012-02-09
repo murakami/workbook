ea23c24deaa506c5972126cf3c3d4694089c2bbb
murakami <m_yukio@mac.com>
2012-02-09 21:23:33 +0900

    2012/02/09

---
+++ ios/Storyboards/Storyboards/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Storyboards
+//
+//  Created by 村上 幸雄 on 12/02/09.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Document : NSObject
+
+@end
---
+++ ios/Storyboards/Storyboards/Document.m
@@ -0,0 +1,13 @@
+//
+//  Document.m
+//  Storyboards
+//
+//  Created by 村上 幸雄 on 12/02/09.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "Document.h"
+
+@implementation Document
+
+@end
