c0a7177d0b94eb0e68150ce0720836b3fc1accea
murakami <m_yukio@mac.com>
2012-02-09 21:29:27 +0900

    2012/02/09

---
+++ ios/Storyboards/Storyboards/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Storyboards
+//
+//  Created by 村上 幸雄 on 12/02/09.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
