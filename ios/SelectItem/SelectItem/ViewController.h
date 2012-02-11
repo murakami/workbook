f9f5e66d9dc0e7b5a5a9356fa21e8647c34c732b
murakami <m_yukio@mac.com>
2012-02-11 23:01:20 +0900

    2012/02/11

--- ios/SelectItem/SelectItem/ViewController.h
+++ ios/SelectItem/SelectItem/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Document.h"
 
 @interface ViewController : UIViewController
+
+@property (strong, nonatomic) Document  *document;
 
 @end
--- ios/SelectItem/SelectItem/ViewController.m
+++ ios/SelectItem/SelectItem/ViewController.m
@@ -6,9 +6,12 @@
 //  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
 //
 
+#import "AppDelegate.h"
 #import "ViewController.h"
 
 @implementation ViewController
+
+@synthesize document = _document;
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,10 +25,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    AppDelegate	*appl = nil;
+	appl = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+	self.document = appl.document;
 }
 
 - (void)viewDidUnload
 {
+    self.document = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
