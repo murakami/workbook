cca1eb409c85171a24a5bf29b59327c9db7ccacc
murakami <m_yukio@mac.com>
2012-02-11 22:39:54 +0900

    select item

---
+++ ios/SelectItem/SelectItemTests/SelectItemTests.h
@@ -0,0 +1,13 @@
+//
+//  SelectItemTests.h
+//  SelectItemTests
+//
+//  Created by 村上 幸雄 on 12/02/11.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SelectItemTests : SenTestCase
+
+@end
---
+++ ios/SelectItem/SelectItemTests/SelectItemTests.m
@@ -0,0 +1,32 @@
+//
+//  SelectItemTests.m
+//  SelectItemTests
+//
+//  Created by 村上 幸雄 on 12/02/11.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "SelectItemTests.h"
+
+@implementation SelectItemTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SelectItemTests");
+}
+
+@end
