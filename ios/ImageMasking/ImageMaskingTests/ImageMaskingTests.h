915a0ac15cd99ee5c34b78818f1c3b4d60f8421b
murakami <m_yukio@mac.com>
2012-02-13 22:02:57 +0900

    2012/02/13

---
+++ ios/ImageMasking/ImageMaskingTests/ImageMaskingTests.h
@@ -0,0 +1,13 @@
+//
+//  ImageMaskingTests.h
+//  ImageMaskingTests
+//
+//  Created by 村上 幸雄 on 12/02/13.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ImageMaskingTests : SenTestCase
+
+@end
---
+++ ios/ImageMasking/ImageMaskingTests/ImageMaskingTests.m
@@ -0,0 +1,32 @@
+//
+//  ImageMaskingTests.m
+//  ImageMaskingTests
+//
+//  Created by 村上 幸雄 on 12/02/13.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "ImageMaskingTests.h"
+
+@implementation ImageMaskingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ImageMaskingTests");
+}
+
+@end
