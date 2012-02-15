6998daeb4952217bfd91f61982b22637b1b322a3
murakami <m_yukio@mac.com>
2012-02-15 23:13:51 +0900

    Coordinate

---
+++ ios/Coordinate/Coordinate/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  Coordinate
+//
+//  Created by 村上 幸雄 on 12/02/15.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
