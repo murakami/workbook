93ddbfab8cec1bc279f49dfb33b56d54aaae6c8b
murakami <m_yukio@mac.com>
2012-02-16 23:36:47 +0900

    2012/02/16

--- ios/Coordinate/Coordinate/MyView.m
+++ ios/Coordinate/Coordinate/MyView.m
@@ -24,6 +24,10 @@
     return self;
 }
 
+#define BEST_BYTE_ALIGNMENT 16
+#define COMPUTE_BEST_BYTES_PER_ROW(bpr)\
+(((bpr) + (BEST_BYTE_ALIGNMENT - 1)) & ~(BEST_BYTE_ALIGNMENT -1))
+
 - (void)awakeFromNib
 {
     DBGMSG(@"%s", __func__);
@@ -35,15 +39,40 @@
     self.upperLeftOriginImage = nil;
 }
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
+    CGContextRef    context = UIGraphicsGetCurrentContext();
+
+    /* LLO(lower-left-origin) */
+    size_t  witdh = rect.size.width;
+    size_t  height = rect.size.height;
+    size_t  bytesPerRow = witdh * 4;
+    bytesPerRow = COMPUTE_BEST_BYTES_PER_ROW(bytesPerRow);
+    unsigned char   *rasterData = calloc(1, bytesPerRow * height);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef    ctx = CGBitmapContextCreate(rasterData, witdh, height,
+                                                8, bytesPerRow,
+                                                colorSpace,
+                                                kCGImageAlphaPremultipliedFirst);
+    CGContextSetRGBStrokeColor(ctx, 1.0, 0.0, 0.0, 1.0);
+    CGContextSetLineWidth(ctx, 4.0);
+    CGContextBeginPath(ctx);
+    CGContextMoveToPoint(ctx, 5.0, 25.0);
+    CGContextAddLineToPoint(ctx, 5.0, 5.0);
+    CGContextDrawPath(ctx, kCGPathStroke);
+    CGContextMoveToPoint(ctx, 5.0, 5.0);
+    CGContextAddLineToPoint(ctx, 25.0, 5.0);
+    CGContextDrawPath(ctx, kCGPathStroke);
+    CGImageRef  cgimage = CGBitmapContextCreateImage(ctx);
+    CGContextDrawImage(context, rect, cgimage);
+    CGContextRelease(ctx);
+    free(rasterData);
+    CGColorSpaceRelease(colorSpace);
+
     /* ULO(upper-left-origin) */
     [self.upperLeftOriginImage drawAtPoint:CGPointMake(10.0, 10.0)];
     
     /* LLO(lower-left-origin) */
-    CGContextRef    context = UIGraphicsGetCurrentContext();
     CGContextSetLineWidth(context, 4.0);
     CGContextBeginPath(context);
     CGContextMoveToPoint(context, 10.0, 30.0);
