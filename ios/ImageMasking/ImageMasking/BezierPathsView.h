0c55fb45a1159ebee16026fa3c3508261b99086a
murakami <m_yukio@mac.com>
2012-02-23 22:08:30 +0900

    Bezier Paths

---
+++ ios/ImageMasking/ImageMasking/BezierPathsView.h
@@ -0,0 +1,13 @@
+//
+//  BezierPathsView.h
+//  ImageMasking
+//
+//  Created by 村上 幸雄 on 12/02/23.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BezierPathsView : UIView
+
+@end
---
+++ ios/ImageMasking/ImageMasking/BezierPathsView.m
@@ -0,0 +1,31 @@
+//
+//  BezierPathsView.m
+//  ImageMasking
+//
+//  Created by 村上 幸雄 on 12/02/23.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "BezierPathsView.h"
+
+@implementation BezierPathsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
