8db364942e476367ed2c418631d3d08e51a18d2d
murakami <m_yukio@mac.com>
2012-02-26 23:45:07 +0900

    WebApp

---
+++ ios/WebApp/WebApp/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  WebApp
+//
+//  Created by 村上 幸雄 on 12/02/26.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIWebView   *webView = (UIWebView *)self.view;
+    NSString    *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    NSURL   *fileURL = [[NSURL alloc] initFileURLWithPath:path];
+    NSURLRequest    *req = [NSURLRequest requestWithURL:fileURL];
+    [webView loadRequest:req];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ ios/WebApp/WebAppTests/WebAppTests.m
@@ -0,0 +1,32 @@
+//
+//  WebAppTests.m
+//  WebAppTests
+//
+//  Created by 村上 幸雄 on 12/02/26.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "WebAppTests.h"
+
+@implementation WebAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WebAppTests");
+}
+
+@end
