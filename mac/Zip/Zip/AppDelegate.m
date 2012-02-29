ccba4359778673e1d137c06e7ff76308ff383351
murakami <m_yukio@mac.com>
2012-02-29 23:39:23 +0900

    2012/02/29

--- mac/Zip/Zip/AppDelegate.m
+++ mac/Zip/Zip/AppDelegate.m
@@ -6,7 +6,12 @@
 //  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
 //
 
+#import "unzip.h"
 #import "AppDelegate.h"
+
+@interface AppDelegate ()
+- (void)unzip:(NSString *)path;
+@end
 
 @implementation AppDelegate
 
@@ -15,6 +20,52 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+- (IBAction)openDocument:(id)sender
+{
+    DBGMSG(@"%s", __func__);
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    /*
+    [panel beginSheetForDirectory:nil
+                             file:nil
+                   modalForWindow:self.window
+                    modalDelegate:self
+                   didEndSelector:@selector(openPanelDidEnd: returnCode: contextInfo:)
+                      contextInfo:NULL];
+    */
+    [panel beginSheetModalForWindow:self.window
+                  completionHandler:^(NSInteger returnCode){
+        NSURL       *pathToFile = nil;
+        NSString    *path = nil;
+        
+        if (returnCode == NSOKButton) {
+            pathToFile = [[panel URLs] objectAtIndex:0];
+            path = [pathToFile path];
+            DBGMSG(@"%@", pathToFile);
+            DBGMSG(@"%@", path);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self unzip:path];
+            });
+        }
+    }];
+}
+
+- (void)unzip:(NSString *)path
+{
+    DBGMSG(@"%s, %@", __func__, path);
+    int error = UNZ_OK;
+    unzFile file = NULL;
+    file = unzOpen([path UTF8String]);
+    unzGoToFirstFile(file);
+    while (error == UNZ_OK) {
+        unz_file_info   fileInfo;
+        char            filename[PATH_MAX];
+        unzGetCurrentFileInfo(file, &fileInfo, filename, PATH_MAX, NULL, 0, NULL, 0);
+        DBGMSG(@"%s", filename);
+        error = unzGoToNextFile(file);
+    }
+    unzClose(file);
 }
 
 @end
