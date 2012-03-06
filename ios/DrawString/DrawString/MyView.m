ecec02fce4e6cb828684e63913a71803a13f305c
murakami <m_yukio@mac.com>
2012-03-06 21:41:19 +0900

    DrawString

---
+++ ios/DrawString/DrawString/MyView.m
@@ -0,0 +1,31 @@
+//
+//  MyView.m
+//  DrawString
+//
+//  Created by 村上 幸雄 on 12/03/06.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
