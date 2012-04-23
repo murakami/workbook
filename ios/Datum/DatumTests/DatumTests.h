8a217df1e48b698cfa98ebb9c935b15362fdc36e
murakami <m_yukio@mac.com>
2012-04-23 22:18:03 +0900

    datum

---
+++ ios/Datum/DatumTests/DatumTests.h
@@ -0,0 +1,13 @@
+//
+//  DatumTests.h
+//  DatumTests
+//
+//  Created by 村上 幸雄 on 12/04/23.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DatumTests : SenTestCase
+
+@end
---
+++ ios/Datum/DatumTests/DatumTests.m
@@ -0,0 +1,32 @@
+//
+//  DatumTests.m
+//  DatumTests
+//
+//  Created by 村上 幸雄 on 12/04/23.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "DatumTests.h"
+
+@implementation DatumTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DatumTests");
+}
+
+@end
