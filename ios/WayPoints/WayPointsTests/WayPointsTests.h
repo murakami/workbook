0afee436c505c40bd8a2c2a76f0b59c72d63c27e
murakami <m_yukio@mac.com>
2012-04-26 22:48:26 +0900

    waypoints

---
+++ ios/WayPoints/WayPointsTests/WayPointsTests.h
@@ -0,0 +1,13 @@
+//
+//  WayPointsTests.h
+//  WayPointsTests
+//
+//  Created by 村上 幸雄 on 12/04/26.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WayPointsTests : SenTestCase
+
+@end
---
+++ ios/WayPoints/WayPointsTests/WayPointsTests.m
@@ -0,0 +1,32 @@
+//
+//  WayPointsTests.m
+//  WayPointsTests
+//
+//  Created by 村上 幸雄 on 12/04/26.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "WayPointsTests.h"
+
+@implementation WayPointsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WayPointsTests");
+}
+
+@end
