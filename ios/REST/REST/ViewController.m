5d229fef9d109759214f00905c2cfcf9a23aac24
murakami <m_yukio@mac.com>
2012-04-29 17:14:16 +0900

    REST

--- ios/REST/REST/ViewController.m
+++ ios/REST/REST/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize textView = _textView;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -22,6 +24,7 @@
 
 - (void)viewDidUnload
 {
+    self.textView = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -29,6 +32,128 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)sendPost:(id)sender
+{
+    NSURL   *url = [NSURL URLWithString:@"http://localhost:3000/people.xml"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPMethod:@"POST"];
+    NSString    *content = @"<person><name>Yukio MURAKAMI</name><age>17</age></person>";
+    [request setHTTPBody:[content dataUsingEncoding:NSUTF8StringEncoding]];
+    NSHTTPURLResponse   *response = nil;
+    NSError *error = nil;
+    NSData  *data = [NSURLConnection sendSynchronousRequest:request
+                                          returningResponse:&response
+                                                      error:&error];
+    if (!error) {
+        NSString    *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSString    *t = [[NSString alloc] initWithFormat:@"status code: %d\ndata: %@", [response statusCode], s];
+        self.textView.text = t;
+        NSLog(@"%@", self.textView.text);
+    }
+    else {
+        NSString    *s = [[NSString alloc] initWithFormat:@"error: %@", error];
+        self.textView.text = s;
+        NSLog(@"%@", self.textView.text);
+    }
+}
+
+- (IBAction)sendGet:(id)sender
+{
+    NSURL   *url = [NSURL URLWithString:@"http://localhost:3000/people/1.xml"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"GET"];
+    NSHTTPURLResponse   *response = nil;
+    NSError *error = nil;
+    NSData  *data = [NSURLConnection sendSynchronousRequest:request
+                                          returningResponse:&response
+                                                      error:&error];
+    if (!error) {
+        NSString    *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSString    *t = [[NSString alloc] initWithFormat:@"status code: %d\ndata: %@", [response statusCode], s];
+        self.textView.text = t;
+        NSLog(@"%@", self.textView.text);
+    }
+    else {
+        NSString    *s = [[NSString alloc] initWithFormat:@"error: %@", error];
+        self.textView.text = s;
+        NSLog(@"%@", self.textView.text);
+    }
+}
+
+- (IBAction)sendGetList:(id)sender
+{
+    NSURL   *url = [NSURL URLWithString:@"http://localhost:3000/people.xml"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"GET"];
+    NSHTTPURLResponse   *response = nil;
+    NSError *error = nil;
+    NSData  *data = [NSURLConnection sendSynchronousRequest:request
+                                          returningResponse:&response
+                                                      error:&error];
+    if (!error) {
+        NSString    *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSString    *t = [[NSString alloc] initWithFormat:@"status code: %d\ndata: %@", [response statusCode], s];
+        self.textView.text = t;
+        NSLog(@"%@", self.textView.text);
+    }
+    else {
+        NSString    *s = [[NSString alloc] initWithFormat:@"error: %@", error];
+        self.textView.text = s;
+        NSLog(@"%@", self.textView.text);
+    }
+}
+
+- (IBAction)sendPut:(id)sender
+{
+    NSURL   *url = [NSURL URLWithString:@"http://localhost:3000/people/1.xml"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPMethod:@"PUT"];
+    NSString    *content = @"<person><name>Yukio MURAKAMI</name><age>96</age></person>";
+    [request setHTTPBody:[content dataUsingEncoding:NSUTF8StringEncoding]];
+    NSHTTPURLResponse   *response = nil;
+    NSError *error = nil;
+    NSData  *data = [NSURLConnection sendSynchronousRequest:request
+                                          returningResponse:&response
+                                                      error:&error];
+    if (!error) {
+        NSString    *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSString    *t = [[NSString alloc] initWithFormat:@"status code: %d\ndata: %@", [response statusCode], s];
+        self.textView.text = t;
+        NSLog(@"%@", self.textView.text);
+    }
+    else {
+        NSString    *s = [[NSString alloc] initWithFormat:@"error: %@", error];
+        self.textView.text = s;
+        NSLog(@"%@", self.textView.text);
+    }
+}
+
+- (IBAction)sendDelete:(id)sender
+{
+    NSURL   *url = [NSURL URLWithString:@"http://localhost:3000/people/1.xml"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPMethod:@"DELETE"];
+    NSHTTPURLResponse   *response = nil;
+    NSError *error = nil;
+    NSData  *data = [NSURLConnection sendSynchronousRequest:request
+                                          returningResponse:&response
+                                                      error:&error];
+    if (!error) {
+        NSString    *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSString    *t = [[NSString alloc] initWithFormat:@"status code: %d\ndata: %@", [response statusCode], s];
+        self.textView.text = t;
+        NSLog(@"%@", self.textView.text);
+    }
+    else {
+        NSString    *s = [[NSString alloc] initWithFormat:@"error: %@", error];
+        self.textView.text = s;
+        NSLog(@"%@", self.textView.text);
+    }
 }
 
 @end
