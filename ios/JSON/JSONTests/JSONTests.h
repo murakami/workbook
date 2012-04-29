711d0a70c9e69e0cbc4eb3d51340da335c4c4a4b
murakami <m_yukio@mac.com>
2012-04-29 19:22:49 +0900

    JSON

---
+++ ios/JSON/JSONTests/JSONTests.h
@@ -0,0 +1,13 @@
+//
+//  JSONTests.h
+//  JSONTests
+//
+//  Created by 村上 幸雄 on 12/04/29.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JSONTests : SenTestCase
+
+@end
---
+++ ios/JSON/JSONTests/JSONTests.m
@@ -0,0 +1,32 @@
+//
+//  JSONTests.m
+//  JSONTests
+//
+//  Created by 村上 幸雄 on 12/04/29.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "JSONTests.h"
+
+@implementation JSONTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JSONTests");
+}
+
+@end
