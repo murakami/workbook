fcb883f6982879455a9849a9c1241e5ca0d13645
murakami <m_yukio@mac.com>
2012-04-29 09:41:46 +0900

    REST

---
+++ ios/REST/RESTTests/RESTTests.h
@@ -0,0 +1,13 @@
+//
+//  RESTTests.h
+//  RESTTests
+//
+//  Created by 村上 幸雄 on 12/04/29.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RESTTests : SenTestCase
+
+@end
---
+++ ios/REST/RESTTests/RESTTests.m
@@ -0,0 +1,32 @@
+//
+//  RESTTests.m
+//  RESTTests
+//
+//  Created by 村上 幸雄 on 12/04/29.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "RESTTests.h"
+
+@implementation RESTTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RESTTests");
+}
+
+@end
