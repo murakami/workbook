a0992d29100e4d4bfcc60dfb753cb85d05532fca
murakami <m_yukio@mac.com>
2012-05-15 00:22:17 +0900

    facemash

---
+++ ios/Facemash/FacemashTests/FacemashTests.h
@@ -0,0 +1,13 @@
+//
+//  FacemashTests.h
+//  FacemashTests
+//
+//  Created by 村上 幸雄 on 12/05/15.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FacemashTests : SenTestCase
+
+@end
---
+++ ios/Facemash/FacemashTests/FacemashTests.m
@@ -0,0 +1,32 @@
+//
+//  FacemashTests.m
+//  FacemashTests
+//
+//  Created by 村上 幸雄 on 12/05/15.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "FacemashTests.h"
+
+@implementation FacemashTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FacemashTests");
+}
+
+@end
