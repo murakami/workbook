eff88309a0b4710b499b8d2d693cad3525d233cf
murakami <m_yukio@mac.com>
2012-05-15 21:27:39 +0900

    facemash

--- ios/Facemash/Facemash/AppDelegate.h
+++ ios/Facemash/Facemash/AppDelegate.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FBConnect.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, FBSessionDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
