f3deebb427009ba32db1769968fcab0c3ecb0112
murakami <m_yukio@mac.com>
2012-05-23 06:29:58 +0900

    base64

---
+++ mac/base64/base64/Base64.h
@@ -0,0 +1,13 @@
+//
+//  Base64.h
+//  base64
+//
+//  Created by 村上 幸雄 on 12/05/23.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Base64 : NSObject
+
+@end
---
+++ mac/base64/base64/Base64.m
@@ -0,0 +1,13 @@
+//
+//  Base64.m
+//  base64
+//
+//  Created by 村上 幸雄 on 12/05/23.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "Base64.h"
+
+@implementation Base64
+
+@end
