205ae1bd2f1dd715b9115a1cf8ebb9737c445690
murakami <m_yukio@mac.com>
2012-06-28 23:49:31 +0900

    analog

--- mac/analog/analog/main.m
+++ mac/analog/analog/main.m
@@ -10,12 +10,24 @@
 
 int main(int argc, const char * argv[])
 {
+    @autoreleasepool {        
+        NSFileHandle    *fhi = [NSFileHandle fileHandleWithStandardInput];
+        NSFileHandle    *fho = [NSFileHandle fileHandleWithStandardOutput];
 
-    @autoreleasepool {
+        NSData  *datainput = [fhi readDataToEndOfFile];
+        NSString    *str = [[NSString alloc] initWithData:datainput encoding:NSUTF8StringEncoding];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSError *error = NULL;
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(.+)\n|(.+)"
+                                                                               options:NSRegularExpressionCaseInsensitive
+                                                                                 error:&error];
+        NSArray    *array = [regex matchesInString:str options:0 range:NSMakeRange(0, str.length)];
+        NSTextCheckingResult    *matches;
+        for (matches in array) {
+            NSString    *s = [str substringWithRange:[matches rangeAtIndex:0]];
+            NSData  *dataout = [[NSData alloc] initWithBytes:[s UTF8String] length:[s lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+            [fho writeData:dataout];
+        }
     }
     return 0;
 }
