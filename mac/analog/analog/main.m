2b5eaa388549235ad6d7da4b0645a5589f144482
murakami <m_yukio@mac.com>
2012-06-28 21:51:18 +0900

    analog

---
+++ mac/analog/analog/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  analog
+//
+//  Created by 村上 幸雄 on 12/06/28.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
