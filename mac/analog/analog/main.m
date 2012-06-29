37b3da5fbc7fa7e885212c9ee6a532fd0328855d
murakami <m_yukio@mac.com>
2012-06-29 23:00:30 +0900

    analog

--- mac/analog/analog/main.m
+++ mac/analog/analog/main.m
@@ -11,22 +11,48 @@
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {        
+#ifndef DEBUG
         NSFileHandle    *fhi = [NSFileHandle fileHandleWithStandardInput];
+#endif
         NSFileHandle    *fho = [NSFileHandle fileHandleWithStandardOutput];
 
+#ifndef DEBUG
         NSData  *datainput = [fhi readDataToEndOfFile];
         NSString    *str = [[NSString alloc] initWithData:datainput encoding:NSUTF8StringEncoding];
+#else
+        NSString    *str = @"12:23:45 start\n1,10\n2,13";
+#endif
         
         NSError *error = NULL;
         NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(.+)\n|(.+)"
                                                                                options:NSRegularExpressionCaseInsensitive
                                                                                  error:&error];
-        NSArray    *array = [regex matchesInString:str options:0 range:NSMakeRange(0, str.length)];
+        NSArray    *array = [regex matchesInString:str
+                                           options:0
+                                             range:NSMakeRange(0, str.length)];
         NSTextCheckingResult    *matches;
         for (matches in array) {
-            NSString    *s = [str substringWithRange:[matches rangeAtIndex:0]];
-            NSData  *dataout = [[NSData alloc] initWithBytes:[s UTF8String] length:[s lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+            NSString    *line = [str substringWithRange:[matches rangeAtIndex:0]];
+
+#ifdef DEBUG
+            NSData  *dataout = [[NSData alloc] initWithBytes:[line UTF8String]
+                                                      length:[line lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
             [fho writeData:dataout];
+#endif
+            
+            NSCharacterSet  *charSet = [NSCharacterSet newlineCharacterSet];
+            line = [line stringByTrimmingCharactersInSet:charSet];
+
+            regex = [NSRegularExpression regularExpressionWithPattern:@"(\\d\\d:\\d\\d:\\d\\d)"
+                                                              options:NSRegularExpressionCaseInsensitive
+                                                                error:&error];
+            NSTextCheckingResult    *match = [regex firstMatchInString:line
+                                                               options:0
+                                                                 range:NSMakeRange(0, line.length)];
+            if (0 < [match numberOfRanges]) {
+                NSString    *tm = [line substringWithRange:[match rangeAtIndex:0]];
+                NSLog(@"time: %@", tm);
+            }
         }
     }
     return 0;
