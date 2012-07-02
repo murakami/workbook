1d10bdd524287d41683b65e1e1dd36bdacf3fc06
murakami <m_yukio@mac.com>
2012-07-02 09:28:13 +0900

    WebApp

--- ios/WebApp/WebApp/AppDelegate.m
+++ ios/WebApp/WebApp/AppDelegate.m
@@ -15,6 +15,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    DBGMSG(@"%s, launchOptions:%@", __func__, launchOptions);
     return YES;
 }
 							
@@ -22,27 +23,59 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    DBGMSG(@"%s", __func__);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    DBGMSG(@"%s", __func__);
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    DBGMSG(@"%s", __func__);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    DBGMSG(@"%s", __func__);
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    DBGMSG(@"%s", __func__);
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    DBGMSG(@"%s, url(%@)", __func__, url);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"demo"
+                                                    message:@"WebApp"
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil, nil];
+    [alert show];
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    DBGMSG(@"%s, url(%@)", __func__, url);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"demo"
+                                                    message:@"WebApp"
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil, nil];
+    [alert show];
+    return YES;
 }
 
 @end
