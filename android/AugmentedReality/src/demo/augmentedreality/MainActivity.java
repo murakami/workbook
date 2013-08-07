6e35b8ae25507015f9b2ab0626d8ae781f3264fd
murakami <m_yukio@mac.com>
2013-08-07 23:01:03 +0900

    AugmentedReality

--- android/AugmentedReality/src/demo/augmentedreality/MainActivity.java
+++ android/AugmentedReality/src/demo/augmentedreality/MainActivity.java
@@ -2,7 +2,9 @@ package demo.augmentedreality;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+		    Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+		    finish();
+		}
 	}
 
 	@Override
