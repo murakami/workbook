f857871f3366e16e1ed50eda558f38f7691a5de7
Yukio MURAKAMI <m_yukio@mac.com>
2014-05-05 00:39:14 +0900

    pipe

--- mac/pipe/pipe/main.m
+++ mac/pipe/pipe/main.m
@@ -12,10 +12,24 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        /* ディレクトリ/配下の一覧を取得するプロセスを用意 */
+        NSTask *task = [[NSTask alloc] init];
+        [task setLaunchPath:@"/bin/ls"];
+        [task setArguments:[NSArray arrayWithObject:@"/"]];
+
+        /* 渡されたデータを読み取るパイプを用意 */
+        NSPipe *pipe = [[NSPipe alloc] init];
+        NSFileHandle *readEnd = [pipe fileHandleForReading];
+
+        /* 先ほどのプロセスの標準出力にパイプを繋げる */
+        [task setStandardOutput:pipe];
+
+        /* 実行 */
+        [task launch];
+
+        /* パイプに渡されたデータを印字 */
+        NSData *stdOutData = [readEnd availableData];
+        NSLog(@"%@", [[NSString alloc] initWithData:stdOutData encoding:NSUTF8StringEncoding]);
     }
     return 0;
 }
