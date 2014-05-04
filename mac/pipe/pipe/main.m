37e6050ae60aa259f972ef9fa9444260c9866031
Yukio MURAKAMI <m_yukio@mac.com>
2014-05-05 00:22:06 +0900

    pipe

---
+++ mac/pipe/pipe/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pipe
+//
+//  Created by 村上幸雄 on 2014/05/05.
+//  Copyright (c) 2014年 村上幸雄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
