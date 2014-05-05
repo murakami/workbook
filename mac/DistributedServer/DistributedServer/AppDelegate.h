ff97af4ab4a86a7ba09412e01aafa7217dff5f28
Yukio MURAKAMI <m_yukio@mac.com>
2014-05-05 19:19:04 +0900

    DistributedServer

---
+++ mac/DistributedServer/DistributedServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DistributedServer
+//
+//  Created by 村上幸雄 on 2014/05/05.
+//  Copyright (c) 2014年 村上幸雄. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ mac/DistributedServer/DistributedServer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DistributedServer
+//
+//  Created by 村上幸雄 on 2014/05/05.
+//  Copyright (c) 2014年 村上幸雄. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
