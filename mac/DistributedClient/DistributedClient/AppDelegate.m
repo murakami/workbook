401f8b10efb04132182922d46db4a00241f75caa
Yukio MURAKAMI <m_yukio@mac.com>
2014-05-05 21:55:50 +0900

    DistributedClient

--- mac/DistributedClient/DistributedClient/AppDelegate.m
+++ mac/DistributedClient/DistributedClient/AppDelegate.m
@@ -8,11 +8,26 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate ()
+- (void)_postNotes;
+@end
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    NSLog(@"%s", __func__);
     // Insert code here to initialize your application
+    
+    [self _postNotes];
+}
+
+- (void)_postNotes
+{
+    NSLog(@"%s", __func__);
+    NSDistributedNotificationCenter *dnc = [NSDistributedNotificationCenter defaultCenter];
+    [dnc postNotificationName:@"DemoDistributedNote"
+                       object:nil];
 }
 
 @end
--- mac/DistributedServer/DistributedServer/AppDelegate.m
+++ mac/DistributedServer/DistributedServer/AppDelegate.m
@@ -8,11 +8,34 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate ()
+- (void)_registerForNotes;
+- (void)_handleDistributedNote:(NSNotification *)note;
+@end
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    NSLog(@"%s", __func__);
     // Insert code here to initialize your application
+    
+    [self _registerForNotes];
+}
+
+- (void)_registerForNotes
+{
+    NSLog(@"%s", __func__);
+    NSDistributedNotificationCenter *dnc = [NSDistributedNotificationCenter defaultCenter];
+    [dnc addObserver:self
+            selector:@selector(_handleDistributedNote:)
+                name:@"DemoDistributedNote"
+              object:nil];
+}
+
+- (void)_handleDistributedNote:(NSNotification *)note
+{
+    NSLog(@"%s Recieived Distributed Notification!:%@", __func__, note);
 }
 
 @end
