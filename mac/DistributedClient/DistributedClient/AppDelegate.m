b233baad447c37163584ecdf91e7150f3f7f4658
Yukio MURAKAMI <m_yukio@mac.com>
2014-05-05 19:18:44 +0900

    DistributedClient

---
+++ mac/DistributedClient/DistributedClient/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  DistributedClient
+//
+//  Created by 村上幸雄 on 2014/05/05.
+//  Copyright (c) 2014年 村上幸雄. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
