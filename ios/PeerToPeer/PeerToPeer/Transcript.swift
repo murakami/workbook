c3d2c32c2ea04ece32e14f257c1388869efd5f97
MURAKAMI Yukio <yukio.murakami@gmail.com>
2015-03-01 16:36:16 +0900

    SequenceGrabber

---
+++ ios/PeerToPeer/PeerToPeer/Transcript.swift
@@ -0,0 +1,9 @@
+//
+//  Transcript.swift
+//  PeerToPeer
+//
+//  Created by 村上幸雄 on 2015/01/31.
+//  Copyright (c) 2015年 村上幸雄. All rights reserved.
+//
+
+import Foundation
---
+++ ios/SequenceGrabber/SequenceGrabber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SequenceGrabber
+//
+//  Created by 村上幸雄 on 2015/03/01.
+//  Copyright (c) 2015年 MURAKAMI Yukio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
