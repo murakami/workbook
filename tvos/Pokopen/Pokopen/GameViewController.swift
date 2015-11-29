da417a85eb8b1fb7460601f76f1d9e28d9b78fd8
村上幸雄 <yukio@mbpci7.local>
2015-11-29 19:40:06 +0900

    Pokopen

---
+++ tvos/Pokopen/Pokopen/GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  Pokopen
+//
+//  Created by 村上幸雄 on 2015/11/29.
+//  Copyright (c) 2015年 MURAKAMI Yukio. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed: "GameScene") {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
