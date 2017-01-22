64ade4f4cd656102c8388e0428891e3bc0502027
Yukio MURAKAMI <m_yukio@mac.com>
2017-01-22 23:58:19 +0900

    RandomApp

---
+++ mac/RandomApp/RandomApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomApp
+//
+//  Created by 村上幸雄 on 2017/01/22.
+//  Copyright © 2017年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
