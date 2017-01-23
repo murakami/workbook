2f2e0475866a93b8d592c52397a85de63ac308c2
Yukio MURAKAMI <m_yukio@mac.com>
2017-01-23 22:42:45 +0900

    RandomApp

--- mac/RandomApp/RandomApp/ViewController.swift
+++ mac/RandomApp/RandomApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import GameplayKit
 
 class ViewController: NSViewController {
 
@@ -21,7 +22,17 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBOutlet var textField: NSTextField?
 
+    @IBAction func generate(sender: NSButton) {
+        let randomNumber = GKRandomSource.sharedRandom().nextInt(upperBound: 99) + 1
+        textField?.integerValue = randomNumber
+    }
+    
+    @IBAction func seed(sender: NSButton) {
+        textField?.stringValue = "Generator seeded"
+    }
 
 }
 
