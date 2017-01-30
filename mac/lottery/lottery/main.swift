0e009b08becb5ac19703b2d81aa275d91238c152
Yukio MURAKAMI <m_yukio@mac.com>
2017-01-30 23:27:25 +0900

    lottery

---
+++ mac/lottery/lottery/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  lottery
+//
+//  Created by 村上幸雄 on 2017/01/30.
+//  Copyright © 2017年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+var array = [Int]()
+for i in 0..<10 {
+    let number = i * 3
+    array.append(number)
+}
+print(array)
+
+exit(EXIT_SUCCESS)
+
+/* End Of File */
