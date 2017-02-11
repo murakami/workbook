32e0f230ce4cf00fa0b81ee1840e9a673f1e57eb
Yukio MURAKAMI <m_yukio@mac.com>
2017-02-11 22:07:27 +0900

    RaiseMan

---
+++ mac/RaiseMan/RaiseMan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RaiseMan
+//
+//  Created by 村上幸雄 on 2017/02/09.
+//  Copyright © 2017年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
