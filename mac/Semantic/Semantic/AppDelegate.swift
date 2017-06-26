e9bbd23b22bdf41976cbb78fb0c176d07b490042
Yukio MURAKAMI <m_yukio@mac.com>
2017-06-27 00:56:56 +0900

    Semantic

---
+++ mac/Semantic/Semantic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Semantic
+//
+//  Created by 村上幸雄 on 2017/06/27.
+//  Copyright © 2017年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac/Semantic/Semantic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Semantic
+//
+//  Created by 村上幸雄 on 2017/06/27.
+//  Copyright © 2017年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
