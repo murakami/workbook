e38160da24902c796be8181dcdcfbecbbfd48072
Yukio MURAKAMI <m_yukio@mac.com>
2017-12-10 19:14:21 +0900

    Destroyer

---
+++ mac/Destroyer/Destroyer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Destroyer
+//
+//  Created by 村上幸雄 on 2017/12/10.
+//  Copyright © 2017年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
