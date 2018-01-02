dab6de857d8929bb31ed54db35f9dd91a8a5cfc3
Yukio MURAKAMI <m_yukio@mac.com>
2018-01-02 17:21:16 +0900

    Crimson

---
+++ mac/Crimson/Crimson/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Crimson
+//
+//  Created by 村上幸雄 on 2018/01/02.
+//  Copyright © 2018年 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
