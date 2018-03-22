0ab2084e3458165f56abc11c144ec763fbae5220
Yukio MURAKAMI <m_yukio@mac.com>
2018-03-22 19:26:41 +0900

    blockchain

---
+++ blockchain/blockchain.swift
@@ -0,0 +1,36 @@
+#!/usr/bin/env xcrun swift
+
+import Foundation
+
+class Blockchain {
+	func registerNode() {
+	}
+
+	func validChain() {
+	}
+
+	func resolveConflicts() {
+	}
+
+	func newBlock() {
+	}
+
+	func newTransaction() {
+	}
+
+	func lastBlock() {
+	}
+
+	func hash() {
+	}
+
+	func proofOfWork() {
+	}
+
+	func validProof() {
+	}
+}
+
+print("hello, world!")
+
+/* End Of File */
