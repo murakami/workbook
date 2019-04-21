c1b1a5898d7c9ba755fdc68e729df72cd5ff43bb
Yukio MURAKAMI <m_yukio@mac.com>
2019-04-21 21:46:56 +0900

    HelloWorld

---
+++ ios/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 村上幸雄 on 2019/04/21.
+//  Copyright © 2019 Bitz Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- mac/MorphologicalAnalysis/MorphologicalAnalysis/ViewController.swift
+++ mac/MorphologicalAnalysis/MorphologicalAnalysis/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        analyzeNaturalLanguageText()
     }
 
     override var representedObject: Any? {
@@ -21,7 +22,18 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    func analyzeNaturalLanguageText() {
+        let text = "東京は、哀しい活気を呈していた、とさいしょの書き出しの一行に書きしるすというような事になるのではあるまいか、と思って東京に舞い戻って来たのに、私の眼には、何の事も無い相変らずの「東京生活」のごとくに映った。"
+        let linguisticTagger = NSLinguisticTagger(tagSchemes: NSLinguisticTagger.availableTagSchemes(forLanguage: "ja"), options: 0)
+        linguisticTagger.string = text
+        linguisticTagger.enumerateTags(in: NSRange(location: 0, length: text.count),
+                             scheme: NSLinguisticTagScheme.tokenType,
+                             options: [.omitWhitespace]) {
+                                tag, tokenRange, sentenceRange, stop in
+                                let subString = (text as NSString).substring(with: tokenRange)
+                                print("\(subString) : \(String(describing: tag))")
+        }
+    }
 }
 
