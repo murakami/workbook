2e5e5e635a1c24411822df086f1b01029104e5e0
Yukio MURAKAMI <m_yukio@mac.com>
2019-07-20 19:05:14 +0900

    Temperature

---
+++ mac/Temperature/Temperature/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Temperature
+//
+//  Created by 村上幸雄 on 2019/07/20.
+//  Copyright © 2019 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac/Temperature/Temperature/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Temperature
+//
+//  Created by 村上幸雄 on 2019/07/20.
+//  Copyright © 2019 Bitz Co., Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
