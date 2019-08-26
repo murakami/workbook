50cb2cbc6b756f0b2262f47849767baab26a14e6
Yukio MURAKAMI <m_yukio@mac.com>
2019-08-27 01:15:13 +0900

    Tweets

--- ios/Tweets/Tweets/ViewController.swift
+++ ios/Tweets/Tweets/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func intentTweet(_ sender : Any) {
+        let text = "Web Intentの例"
+        let encodedText = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        if let encodedText = encodedText,
+            let url = URL(string: "https://twitter.com/intent/tweet?text=\(encodedText)") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+
+    @IBAction func activityTweet(_ sender : Any) {
+        let text = "共有機能を利用する"
+        let bundlePath = Bundle.main.path(forResource: "brownout", ofType: "jpg")
+        let image = UIImage(contentsOfFile: bundlePath!)
+        let shareItems = [image, text] as [Any]
+        let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+        present(controller, animated: true, completion: nil)
+    }
 
 }
 
