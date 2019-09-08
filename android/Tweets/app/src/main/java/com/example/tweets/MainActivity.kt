f6299222cd104bc18c863a7f3f4ebe511fff2033
Yukio MURAKAMI <m_yukio@mac.com>
2019-09-09 00:20:59 +0900

    Tweets

--- android/Tweets/app/src/main/java/com/example/tweets/MainActivity.kt
+++ android/Tweets/app/src/main/java/com/example/tweets/MainActivity.kt
@@ -2,11 +2,67 @@ package com.example.tweets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import androidx.core.app.ShareCompat
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intentTweetButton: Button = findViewById(R.id.intentTweetButton)
+        intentTweetButton.setOnClickListener {
+            shareTwitter()
+        }
+
+        val shareCompatButton: Button = findViewById(R.id.shareCompatButton)
+        shareCompatButton.setOnClickListener {
+            shareCompat()
+        }
+    }
+
+    fun shareTwitter() {
+        val message = "shareTwitter intent tweet"
+        try {
+            val sharingIntent = Intent(Intent.ACTION_SEND)
+            sharingIntent.setClassName("com.twitter.android", "com.twitter.android.PostActivity")
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(sharingIntent)
+        }
+        catch (e: Exception) {
+            Log.e("In Exception", "Comes here")
+            val i = Intent()
+            i.putExtra(Intent.EXTRA_TEXT, message)
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("https://mobile.twitter.com/compose/tweet")
+            startActivity(i)
+        }
+
+    }
+
+    fun shareCompat() {
+        val message = "shareCompat"
+        val imagePath = "http://www.bitz.co.jp/images/brownout/appstore.jpg"
+
+        val builder = ShareCompat.IntentBuilder.from(this)
+
+        // データをセットする
+        builder.setChooserTitle("Choose App")
+        builder.setText(message)
+        if (imagePath != null) {
+            builder.setType("image/png")
+            builder.addStream(Uri.parse(imagePath))
+        }
+        else {
+            builder.setType("text/plain")
+        }
+        // アプリ選択画面を起動
+        builder.startChooser()
     }
 }
