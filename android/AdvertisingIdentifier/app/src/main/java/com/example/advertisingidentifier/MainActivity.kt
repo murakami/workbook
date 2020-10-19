c5ed707f5a730895b41833a81e7248c65fb8fdcb
Yukio Murakami <murakami@bitz.co.jp>
2020-10-19 20:08:54 +0900

    AdvertisingIdentifier

---
+++ android/AdvertisingIdentifier/app/src/main/java/com/example/advertisingidentifier/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.advertisingidentifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.ads.identifier.AdvertisingIdClient
+import androidx.ads.identifier.AdvertisingIdInfo
+import com.google.common.util.concurrent.FutureCallback
+
+// Used for the call to addCallback() within this snippet.
+import com.google.common.util.concurrent.Futures.addCallback
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity", "onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        determineAdvertisingInfo()
+    }
+
+    private fun determineAdvertisingInfo() {
+        if (AdvertisingIdClient.isAdvertisingIdProviderAvailable(this)) {
+            Log.d("MainActivity", "isAdvertisingIdProviderAvailable");
+            val advertisingIdInfoListenableFuture =
+                    AdvertisingIdClient.getAdvertisingIdInfo(applicationContext)
+
+            addCallback(advertisingIdInfoListenableFuture,
+                    object : FutureCallback<AdvertisingIdInfo> {
+                        override fun onSuccess(adInfo: AdvertisingIdInfo?) {
+                            val id: String? = adInfo?.getId()
+                            val providerPackageName: String? = adInfo?.getProviderPackageName()
+                            val isLimitTrackingEnabled: Boolean? =
+                                    adInfo?.isLimitAdTrackingEnabled()
+                            Log.d("MainActivity",
+                                "id(" + id + ") providerPackageName(" + providerPackageName + ") isLimitTrackingEnabled(" + isLimitTrackingEnabled + ")")
+                        }
+
+                        override fun onFailure(t: Throwable) {
+                            Log.e("MainActivity",
+                                    "Failed to connect to Advertising ID provider.")
+                            // Try to connect to the Advertising ID provider again, or fall
+                            // back to an ads solution that doesn't require using the
+                            // Advertising ID library.
+                        }
+                    }, Executors.newSingleThreadExecutor())
+        } else {
+            // The Advertising ID client library is unavailable. Use a different
+            // library to perform any required ads use cases.
+            Log.d("MainActivity", "not isAdvertisingIdProviderAvailable")
+        }
+    }
+}
