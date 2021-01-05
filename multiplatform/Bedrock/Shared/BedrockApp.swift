1639afe880ddfd163ad4ef773af608fcaa4ffe41
Yukio Murakami <murakami@bitz.co.jp>
2021-01-05 09:29:37 +0900

    Bedrock

---
+++ multiplatform/Bedrock/Shared/BedrockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BedrockApp.swift
+//  Shared
+//
+//  Created by 村上幸雄 on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct BedrockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
