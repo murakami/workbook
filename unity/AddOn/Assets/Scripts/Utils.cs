ec3d04a309d7b668ec65140825d4868bc5a10141
Yukio Murakami <murakami@bitz.co.jp>
2021-01-19 12:00:44 +0900

    AddOn

--- unity/AddOn/Assets/Scripts/Utils.cs
+++ unity/AddOn/Assets/Scripts/Utils.cs
@@ -15,7 +15,6 @@ public class Utils : MonoBehaviour
         #if UNITY_IPHONE
         print (FooPluginFunction ());
         #elif UNITY_ANDROID
-        //AndroidJavaObject jo = new AndroidJavaObject("com.example.Utils", "fooPluginFunction");
         using (AndroidJavaClass cls = new AndroidJavaClass("com.example.Utils")) {
             Debug.Log("FooPluginFunction: " + cls.CallStatic<double>("fooPluginFunction"));
         }
