1d8539a411b37c010ac19739cc52d404ae08bc29
村上幸雄 <yukio@macmini2014late.lan>
2021-09-06 20:57:00 +0900

    Interviews

---
+++ multiplatform/Interviews/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 村上幸雄 on 2021/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ multiplatform/Interviews/Shared/InterviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewsApp.swift
+//  Shared
+//
+//  Created by 村上幸雄 on 2021/09/06.
+//
+
+import SwiftUI
+
+@main
+struct InterviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
