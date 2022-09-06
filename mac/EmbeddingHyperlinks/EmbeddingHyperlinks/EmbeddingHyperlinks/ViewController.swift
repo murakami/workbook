4951cdf2d2d9867cb7c77d864248b122aa99a16b
murakami <m_yukio@mac.com>
2022-09-06 18:23:54 +0900

    EmbeddingHyperlinks

---
+++ mac/EmbeddingHyperlinks/EmbeddingHyperlinks/EmbeddingHyperlinks/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EmbeddingHyperlinks
+//
+//  Created by 村上幸雄 on 2022/09/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
