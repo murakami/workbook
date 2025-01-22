40083663a33c553da918158dcb1a816a1a83099a
Yukio Murakami <m_yukio@mac.com>
2025-01-22 15:14:27 +0900

    SpeakLine

---
+++ multiplatform/SpeakLine/SpeakLine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeakLine
+//
+//  Created by 村上幸雄 on 2025/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multiplatform/SpeakLine/SpeakLine/SpeakLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakLineApp.swift
+//  SpeakLine
+//
+//  Created by 村上幸雄 on 2025/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SpeakLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
