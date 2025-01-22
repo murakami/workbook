60f5532ed46f9f4b476155cc4690f08ca8480b32
Yukio Murakami <m_yukio@mac.com>
2025-01-22 15:15:12 +0900

    SpeakLine

--- multiplatform/SpeakLine/SpeakLine/ContentView.swift
+++ multiplatform/SpeakLine/SpeakLine/ContentView.swift
@@ -6,14 +6,44 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var text = ""
+    private let speechSynth = AVSpeechSynthesizer()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack(alignment: .topLeading) {
+                TextEditor(text: $text)
+                
+                // prompt
+                if text.isEmpty {
+                    Text("Enter text to be spoken...") .foregroundColor(Color.gray)
+                        .padding(.horizontal, 6)
+                }
+            }
+            HStack {
+                Spacer()
+                Button("Stop") {
+                    print("stop button clicked")
+                    speechSynth.stopSpeaking(at: AVSpeechBoundary.immediate)
+                }
+                Button("Speak") {
+                    if text.isEmpty {
+                        print("string from TextEditor is empty")
+                    } else {
+                        print("string is \(text)")
+                        let utterance = AVSpeechUtterance(string: text)
+                        let locale = Locale(identifier:Locale.preferredLanguages[0])
+                        print("locale is \(locale)")
+                        let code = locale.languageCode!
+                        print("code is \(code)")
+                        utterance.voice = AVSpeechSynthesisVoice(language: code)
+                        speechSynth.speak(utterance)
+                    }
+                }
+            }
         }
         .padding()
     }
