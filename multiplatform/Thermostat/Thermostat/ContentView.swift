e32249600c94bb9780e53756cfca2c8b32a242e8
Yukio Murakami <m_yukio@mac.com>
2025-01-29 22:50:30 +0900

    Thermostat

--- multiplatform/Thermostat/Thermostat/ContentView.swift
+++ multiplatform/Thermostat/Thermostat/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var internalTemperature: Double = 68.0
+    @State private var isOn = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack {
+            VStack {
+                Slider(value: $internalTemperature, in: 0 ... 212, step: 1.0)
+                    .disabled(!isOn)
+                Text(String(format: "%.0lf", internalTemperature))
+            }
+            .padding()
+            VStack {
+                Spacer()
+                Button("Warmer") {
+                    internalTemperature += 1.0
+                }
+                .disabled(!isOn)
+                Spacer()
+                Button("Cooler") {
+                    internalTemperature += -1.0
+                }
+                .disabled(!isOn)
+                Spacer()
+                Button("Power") {
+                    isOn = !isOn
+                }
+                .buttonStyle(.borderless)
+            }
+            .padding()
+        }.padding()
     }
 }
 
