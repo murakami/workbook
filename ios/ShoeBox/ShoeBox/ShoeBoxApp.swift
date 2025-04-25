84088adb90e5883abe1ad2dd4afc28bcb53eade5
Yukio Murakami <m_yukio@mac.com>
2025-04-25 14:39:45 +0900

    ShoeBox

---
+++ ios/ShoeBox/ShoeBox/ShoeBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeBoxApp.swift
+//  ShoeBox
+//
+//  Created by 村上幸雄 on 2025/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoeBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
